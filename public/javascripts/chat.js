f4c77a202b44908dc03d90b4a5c1f6cd4b35d9e5
Priyesh Mistry <Priyesh Mistry>
2017-03-14 16:39:59 +0000

    up to page 34

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -14,3 +14,28 @@ Chat.prototype.changeRoom = function(room){
         newRoom: room
     });
 };
+
+Chat.prototype.processCommand = function(command){
+    var words = command.split(' ');
+    var command = words[0]
+                    .substring(1, words[0].length)
+                    .toLoverCase();
+    var message = false;
+    
+    switch(command){
+        case 'join':
+            words.shift();
+            var room = words.join(' ');
+            this.changeRoom(room);
+            break;
+        case 'nick':
+            words.shift();
+            var name = words.join(' ');
+            this.socket.emit('nameAttempt');
+            break;
+        default:
+            message = 'Unrecognized command.';
+            break;
+    }
+    return message;
+}
